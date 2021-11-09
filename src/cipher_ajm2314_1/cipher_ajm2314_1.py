e6f66c9cbfe6241106670cadc4b6a9c9a3672f91
JohnMarion54 <90579245+JohnMarion54@users.noreply.github.com>
2021-11-08 19:21:02 -0500

    Update cipher_ajm2314_1.py

--- src/cipher_ajm2314_1/cipher_ajm2314_1.py
+++ src/cipher_ajm2314_1/cipher_ajm2314_1.py
@@ -1,4 +1,33 @@
 def cipher(text, shift, encrypt=True):
+    """
+    Encrypts and decrypts phrases by moving each alphabetical character a given number of index units.
+
+    Parameters
+    ----------
+    text: A string to be encrypted or decrypted. Can include alphabetical or other characters 
+    shift: The number of index units to shift each alphabetical character. 
+        An integer
+    encrypt: If True, alphabetical characters will be shifted by the given value for the shift argument.
+        If false, the alphabetical characters will be shifted by the negative of the given value for the shift argument.
+    
+    Returns
+    ----------
+    string
+        The encrypted or decrypted string.
+
+    Examples
+    ----------
+    >>> import cipher_ajm2314
+    >>> cipher('Mr. Smith Goes to Washington',1)
+        'Ns. Tnjui Hpft up Xbtijohupo'
+
+    >>> cipher('Mr. Smith Goes to Washington',-3)
+        'Jo. Pjfqe Dlbp ql TXpefkdqlk'
+
+    >>> cipher('Mr. Smith Goes to Washington',3,encrypt=False)
+        'Jo. Pjfqe Dlbp ql TXpefkdqlk'
+    """
+    
     alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
     new_text = ''
     for c in text:
