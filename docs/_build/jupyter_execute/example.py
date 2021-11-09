d3ef89de9d42adf35e7a60c347902a669a719beb
John Marion <ajm2314@columbia.edu>
2021-11-08 23:18:11 -0500

    Changed .gitignore

---
+++ docs/_build/jupyter_execute/example.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Example usage
+# 
+# To use `cipher_ajm2314_1` in a project:
+
+# In[1]:
+
+
+import cipher_ajm2314_1
+
+print(cipher_ajm2314_1.__version__)
+
